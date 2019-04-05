b1a324812348896135f6381d712d786c64f6a12e
oli-gra <oliver.grant@gmail.com>
2019-04-05 17:50:21 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'students/no-test--framework'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :students
 end
